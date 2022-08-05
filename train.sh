e51e6ffe358e418ae5380e8b38193128b19c0244
Herman <zhanghm_1995@qq.com>
2022-08-05 14:27:32 +0800

    update num_workers

--- train.sh
+++ train.sh
@@ -1,5 +1,20 @@
 set -x
 
-python -m torch.distributed.launch --nproc_per_node=1 --master_port 12345 train.py \
---config ./config/face.yaml \
+ZHM_PATH="/221019051/"
+YZH_PATH="/221019041/"
+
+PYTHON=python
+if [ -d "$ZHM_PATH" -o -d "$YZH_PATH" ]; then
+    echo "In AIStation platform"
+    PYTHON=/root/miniconda3/envs/py36-torch100-cu11/bin/python
+    if [ -f "$PYTHON" ]; then
+        PYTHON=/root/miniconda3/envs/py36-torch100-cu11/bin/python
+    else
+        PYTHON=python
+    fi
+    cd /221019051/Research/Face/hififace
+fi
+
+${PYTHON} -m torch.distributed.launch --nproc_per_node=4 --master_port 12345 train.py \
+--config ./config/face_ours.yaml \
 --name face_vox
