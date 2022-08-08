8323674a52d36e8942e559741409a2f21c94e752
Herman <zhanghm_1995@qq.com>
2022-08-08 14:23:38 +0800

    add test_model.py

--- inference_face_to_face.sh
+++ inference_face_to_face.sh
@@ -6,7 +6,7 @@ set -x
 ########## For cross identity ##########
 python inference_face_to_face.py \
 --config ./config/face_to_face.yaml \
---name face_to_face_vox_l1_loss \
+--name face_to_face_vox_l1_loss_new_mask \
 --no_resume \
---output_dir ./vox_result/debug \
+--output_dir ./vox_result/trainset_fixed_reference_image_another_exp_erode_11 \
 --single_gpu
